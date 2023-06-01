52742c17590e0eb1331158ea26b665488b6d9308
Paola Arrieta <vargasp437@gmail.com>
2023-06-01 08:17:43 -0600

    adding more components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 
 import Navbar from './components/Navbar'
 import Inicio from './components/Inicio'
-import NextArrow from './components/NextArrow';
 import Historia from './components/Historia';
 import Galeria from './components/Galeria';
-import Menu from './components/menu';
+import Menu from './components/Menu';
 import Contacto from './components/Contacto';
-import {FaUser} from "react-icons/fa";
 import Footer from './components/Footer';
 import ContactScreen from './components/ContactScreen';
 import Nosotros from './components/Nosotros';
@@ -16,20 +14,15 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   
   return (
-    <div>
-    <Navbar/>
-    <Inicio/>
-    <NextArrow/>
-    <Historia/> 
-    <NextArrow/>
-    <Galeria/>
-    <NextArrow/>
-    <Menu/>
-    <NextArrow/>
-    <Contacto/>
-    <Nosotros/>
-    <Footer/>
-    </div>
+    <>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<div> <Inicio/><Historia/><Galeria/><Menu/><Contacto/> </div>}></Route>
+        <Route path='Nosotros' element={<Nosotros/>}/>
+        <Route path='Contacto' element={<ContactScreen/>}/>
+      </Routes>
+      <Footer/>
+    </>
   );
 }
 
