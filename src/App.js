02d15a90ad6ee3d56e7656f179aa079170d66d4a
Paola Arrieta <vargasp437@gmail.com>
2023-05-31 22:51:07 -0600

    adding route paths

--- src/App.js
+++ src/App.js
@@ -4,18 +4,39 @@ import Inicio from './components/Inicio'
 import NextArrow from './components/NextArrow';
 import Historia from './components/Historia';
 import Galeria from './components/Galeria';
-import Menu from './components/menu';
+import Menu from './components/Menu';
 import Contacto from './components/Contacto';
 import {FaUser} from "react-icons/fa";
 import Footer from './components/Footer';
 import ContactScreen from './components/ContactScreen';
 import Nosotros from './components/Nosotros';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 
 function App() {
   
   return (
+    <><Navbar /><Routes>
+
+      <Route path='/' element={<div><Inicio/><Historia/><Galeria/><Menu/><Contacto/></div>}> </Route>
+      <Route path='Nosotros' element={<Nosotros/>}/>
+      <Route path='ContactScreen' element={<ContactScreen/>}/>
+    </Routes>
+    <Footer/>
+    </>
+    
+  );
+}
+
+
+
+export default App;
+
+/*  
+    <Inicio/>
+    <Historia/> 
+    <Galeria/>  
+
     <div>
     <Navbar/>
     <Inicio/>
@@ -30,16 +51,5 @@ function App() {
     <Nosotros/>
     <Footer/>
     </div>
-  );
-}
-
-
-
-export default App;
-
-/*  
-    <Inicio/>
-    <Historia/> 
-    <Galeria/>   
 
     */
--- src/components/ContactScreen.js
+++ src/components/ContactScreen.js
@@ -1,5 +1,6 @@
 import {FaWaze } from "react-icons/fa";
 import {FaMapMarkerAlt} from "react-icons/fa";
+import Contacto from "./Contacto";
 
 
 function ContactScreen(){
@@ -9,10 +10,19 @@ function ContactScreen(){
         <section>
             <h1>Palmares, Alajuela</h1>
             <p>Detras del redondel de Palmares<br/>200 metros sur</p>
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d491.0615224122973!2d-84.43535439670086!3d10.05869746055714!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8fa04539d5d5b5b3%3A0x592a537ebe3dc532!2sOlikey%20Coffee%20Roasters!5e0!3m2!1ses!2scr!4v1684939665538!5m2!1ses!2scr" width="1033" height="443" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-            <div>
-                <FaWaze><a></a></FaWaze>
-                <FaMapMarkerAlt><a></a></FaMapMarkerAlt>
+            <div id="map"> <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d491.0615224122973!2d-84.43535439670086!3d10.05869746055714!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8fa04539d5d5b5b3%3A0x592a537ebe3dc532!2sOlikey%20Coffee%20Roasters!5e0!3m2!1ses!2scr!4v1684939665538!5m2!1ses!2scr" 
+            width="1033" 
+            height="443" 
+            style={{border:"0"}} 
+            allowfullscreen="" 
+            loading="lazy" 
+            referrerpolicy="no-referrer-when-downgrade">
+            </iframe></div>
+           
+            <div id="mapIcons">
+                <a href="https://ul.waze.com/ul?place=ChIJs7XV1TlFoI8RMsU9vn5TKlk&ll=10.05869750%2C-84.43503250&navigate=yes&utm_campaign=default&utm_source=waze_website&utm_medium=lm_share_location"><FaWaze className="iconMaps" ></FaWaze></a>
+                <a href="https://goo.gl/maps/NRzUmkvo4BDhF2rDA"><FaMapMarkerAlt className="iconMaps" ></FaMapMarkerAlt></a>
+                
             </div>
         </section>
         </>
