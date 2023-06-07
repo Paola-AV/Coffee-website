c7422015bb4d46701839ac3cacb7c493b57f7012
Paola-AV <vargasp437@gmail.com>
2023-06-06 20:36:56 -0600

    adding a new menu component

--- src/components/menu.js
+++ src/components/menu.js
@@ -5,7 +5,59 @@ function Menu() {
   return (
     <section id="menu">
       <div><h1 id="tittleMenu">Menu</h1></div>
-      <div id="menuRow">
+      <div className="columnDivMenu">
+
+        <div className="columnMenu">
+          <div className="menuDiv">
+            <div id="menuComponent">
+              <h2 id="compMenuH">Espresso</h2>
+              <p id="compMenuP">Lorem ipsum dolor sit amet</p>
+            </div>
+            <a id="menuBtn">Conoce mas</a>
+            <img id="imgMenu" src="galeriaImg/vasoGaleria.png" />
+          </div>
+
+          <div className="menuDiv">
+            <div id="menuComponent">
+              <h2 id="compMenuH">Infusiones</h2>
+              <p id="compMenuP">Lorem ipsum dolor sit amet</p>
+            </div>
+            <a id="menuBtn">Conoce mas</a>
+            <img id="imgMenu" src="copa1.png" />
+          </div>
+        </div>
+
+        <div className="columnMenu">
+          <div className="menuDiv">
+            <div id="menuComponent">
+              <h2 id="compMenuH">Lattes</h2>
+              <p id="compMenuP">Lorem ipsum dolor sit amet</p>
+            </div>
+            <a id="menuBtn">Conoce mas</a>
+            <img id="imgMenu" src="latte.png" />
+          </div>
+
+          <div className="menuDiv">
+            <div id="menuComponent">
+              <h2 id="compMenuH">metodos de infusion</h2>
+              <p id="compMenuP">Lorem ipsum dolor sit amet</p>
+            </div>
+            <a id="menuBtn">Conoce mas</a>
+            <img id="imgMenu" src="menuPouring.png" />
+          </div>
+
+        </div>
+      </div>
+
+
+      
+    </section>
+
+  )
+}
+
+export default Menu;
+/*<div id="menuRow">
         <div id="menuImgDiv">
           <img src="menuPouring.png" id="menuColImg" />
           
@@ -28,11 +80,4 @@ function Menu() {
 
         </div>
 
-      </div>
-      
-    </section>
-
-  )
-}
-
-export default Menu;
+      </div>*/
