7a9ab663c290444504d0c7372baa4ae2b839621c
Paola Arrieta <vargasp437@gmail.com>
2023-06-08 17:16:01 -0600

    working on responsiveness

--- src/components/Inicio.js
+++ src/components/Inicio.js
@@ -10,19 +10,19 @@ function Inicio() {
             <div class="col-4" id="col1">
                 <div id="txtInicio">
                     <h1>Lorem Ipsum</h1>
-                    <p class="pInicio">Lorem ipsum dolor sit amet,<br/>consectetur adipiscing elit</p>
+                    <p class="pInicio">Lorem ipsum dolor sit amet,<br />consectetur adipiscing elit</p>
                 </div>
                 <div id="butInicio">
 
-                <button onClick={()=>navigate('Nosotros')} class="btnInicio">Nosotros</button>
-                <button onClick={()=>navigate('Contacto')} class="btnInicio" id="btn2">Contacto</button>
+                    <button onClick={() => navigate('Nosotros')} class="btnInicio">Nosotros</button>
+                    <button onClick={() => navigate('Contacto')} class="btnInicio" id="btn2">Contacto</button>
 
                 </div>
-               
+
             </div>
 
             <div class="col-8" id="col2">
-                <img src="Café-y-Quequito.png" id="coffeeImg"/>
+                <img src="Café-y-Quequito.png" id="coffeeImg" />
                 <div id="granosDiv">
                     <img src="Granos-Under-Vaso.png" id="granosImg" />
                 </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,48 @@
 
 import { FaBars, FaTimes } from "react-icons/fa";
-
+import Nav from 'react-bootstrap/Nav';
 
 
 function Navbar() {
 
 
   return (
+
     <header class="navbarHeader">
-      <nav class="navbar navbar-expand-lg bg-body-tertiary" id="navBox">
+      <Nav defaultActiveKey="/home" as="ul">
+
+        <Nav.Item as="li" id="navbarNav">
+          <Nav.Link href="/home" id="li">Inicio</Nav.Link>
+        </Nav.Item>
+
+        <Nav.Item as="li" id="navbarNav">
+          <Nav.Link eventKey="link-1" id="li">Menu</Nav.Link>
+        </Nav.Item>
+
+        <li>
+          <div id="logo-nav"> </div>
+        </li>
+
+        <Nav.Item as="li" id="navbarNav">
+          <Nav.Link eventKey="link-2" id="li">Historia</Nav.Link>
+        </Nav.Item>
+
+        <Nav.Item as="li" id="navbarNav">
+          <Nav.Link eventKey="link-2" id="li">Contacto</Nav.Link>
+        </Nav.Item>
+
+      </Nav>
+
+    </header>
+
+  )
+
+}
+
+export default Navbar;
+
+/*    <header class="navbarHeader">
+      <nav class="navbar navbar-expand-lg " id="navBox">
         <div class="container-fluid">
 
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -37,11 +71,4 @@ function Navbar() {
           </div>
         </div>
       </nav>
-    </header>
-
-
-  )
-
-}
-
-export default Navbar;
+    </header>*/
