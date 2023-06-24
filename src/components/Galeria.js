fc82c0c38b175e530ed9054f78de0e6e43033eff
Paola Arrieta <vargasp437@gmail.com>
2023-06-23 20:44:55 -0600

    CREATING A NEW GALERY COMPONENT

--- src/components/Galeria.js
+++ src/components/Galeria.js
@@ -1,13 +1,76 @@
 import Carousel from 'react-bootstrap/Carousel';
 import { useNavigate } from "react-router-dom";
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import 'swiper/css';
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+import { EffectCoverflow, Pagination, Navigation } from 'swiper';
+
+import slide_image_1 from '../CarrouselImg/cafeSol.PNG';
+import slide_image_2 from '../CarrouselImg/chemex.PNG';
+import slide_image_3 from '../CarrouselImg/tostadora.PNG';
 
 function Galeria() {
     const navigate=useNavigate()
   return (
-    <section id='galeria'>
-        <div id="tittleGal">
-          <h1 id="tittleGaleria">Galeria</h1>
-      </div><div id="bow"></div>
+    <><section id='galeria'>
+          <div id="tittleGal">
+              <h1 id="tittleGaleria">Galeria</h1>
+          </div>
+
+      </section><div className="container">
+              <h1 className="heading">Flower Gallery</h1>
+              <Swiper
+                  effect={'coverflow'}
+                  grabCursor={true}
+                  centeredSlides={true}
+                  loop={true}
+                  slidesPerView={'auto'}
+                  coverflowEffect={{
+                      rotate: 0,
+                      stretch: 0,
+                      depth: 100,
+                      modifier: 2.5,
+                  }}
+                  pagination={{ el: '.swiper-pagination', clickable: true }}
+                  navigation={{
+                      nextEl: '.swiper-button-next',
+                      prevEl: '.swiper-button-prev',
+                      clickable: true,
+                  }}
+                  modules={[EffectCoverflow, Pagination, Navigation]}
+                  className="swiper_container"
+              >
+                  <SwiperSlide>
+                      <img src={slide_image_1} alt="slide_image" />
+                  </SwiperSlide>
+                  <SwiperSlide>
+                      <img src={slide_image_2} alt="slide_image" />
+                  </SwiperSlide>
+                  <SwiperSlide>
+                      <img src={slide_image_3} alt="slide_image" />
+                  </SwiperSlide>
+
+                  <div className="slider-controler">
+                      <div className="swiper-button-prev slider-arrow">
+                          <ion-icon name="arrow-back-outline"></ion-icon>
+                      </div>
+                      <div className="swiper-button-next slider-arrow">
+                          <ion-icon name="arrow-forward-outline"></ion-icon>
+                      </div>
+                      <div className="swiper-pagination"></div>
+                  </div>
+              </Swiper>
+          </div></>
+  );
+}
+
+export default Galeria;
+
+/*<div id="bow"></div>
       <div id='centerC'>
               <Carousel id="carHolder">
                   <Carousel.Item id='carousel'>
@@ -35,14 +98,7 @@ function Galeria() {
                           class="carImg" />
                   </Carousel.Item>
               </Carousel>
-          </div>
-    </section>
-      
-  );
-}
-
-export default Galeria;
-
+          </div>*/
 
 /*function Galeria() {
 
