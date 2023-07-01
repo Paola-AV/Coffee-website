39b9def56ec0fb80979b387af5416f8a0fbce02d
Paola Arrieta <vargasp437@gmail.com>
2023-06-30 22:27:42 -0600

    fixing the components spacing

--- src/components/Nosotros.js
+++ src/components/Nosotros.js
@@ -10,89 +10,65 @@ function Nosotros() {
     return (
         <section id="nosotrosSec">
             <h1 id="aboutUsTittle"><span id="TittleColor1">Nuestra</span> Historia</h1>
-            <div id="aboutRowSwitch">
-                <div id="aboutImg">
-                    <img src="bosque.PNG" id="imgRoundSwitch" />
-                </div>
-                <div data-aos="fade-up">
-                <div id="aboutPSwitch">
-                    <h2 id="aboutTitleSwitch">Lorem Ipsum</h2>
-                    <p id="aboutParagraphSwitch">
-                        Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus.
-                        Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem.
-                    </p>
-                </div>
-                </div>
-                
-            </div>
 
             <div id="aboutRow">
-                <div id="aboutPSwitch">
-                    <h2 id="aboutTitleSwitch">Lorem Ipsum</h2>
-                    <p id="aboutParagraphSwitch">
-                        Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus.
-                        Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem.
-                    </p>
-                </div>
-
-                <div id="aboutP">
+                <div id="aboutP" data-aos="fade-up">
                     <h2 id="aboutTitle">Lorem Ipsum</h2>
                     <p id="aboutParagraph">
                         Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus.
                         Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem.
                     </p>
                 </div>
-                <div id="aboutImg">
-                    <img src="cafetal.PNG" id="imgRound" />
+                <div id="aboutImg" data-aos="fade-left">
+                    <img src="bosque.PNG" id="imgRound" />
 
                 </div>
             </div>
+            
 
             <div id="rowAboutDiv">
 
-                <div id="aboutPMiddle">
+                <div id="aboutPMiddle" data-aos="fade-up">
                     <h2 id="aboutTitle">Lorem Ipsum</h2>
                     <div id="middlePDiv">
-                    <p id="middlePleft">
-                        Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. 
-                    </p>
-                    <p id="middlePright">Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum.</p>
+                        <p id="middlePleft">
+                            Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim.
+                        </p>
+                        <p id="middlePright">Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum.</p>
                     </div>
-                    <div id="imgDivMiddle"><img src="cafeSol.PNG" id="imgRoundMiddle" />
-</div>
+                    <div id="imgDivMiddle" data-aos="fade-left"><img src="cafeSol.PNG" id="imgRoundMiddle" />
+                    </div>
                 </div>
-                    
+
             </div>
 
             <div id="aboutRowSwitch">
-                <div id="aboutImg">
+                <div id="aboutImg" >
                     <img src="oli.PNG" id="imgRoundSwitch" />
 
                 </div>
-                <div id="aboutPSwitch">
+                <div id="aboutPSwitch" data-aos="fade-up">
                     <h2 id="aboutTitleSwitch">Lorem Ipsum</h2>
                     <p id="aboutParagraphSwitch">
                         Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus.
                         Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem.
                     </p>
                 </div>
-                
+
             </div>
 
             <div id="aboutRow">
-                <div id="aboutP">
+                <div id="aboutP" data-aos="fade-up">
                     <h2 id="aboutTitle">Lorem Ipsum</h2>
                     <p id="aboutParagraph">
                         Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus.
                         Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem.
                     </p>
                 </div>
-                <div id="aboutImg">
+                <div id="aboutImg" data-aos="fade-left">
                     <img src="tostadora.PNG" id="imgRound" />
 
                 </div>
-
-
             </div>
 
         </section>
